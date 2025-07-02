bbe1c6b6bb465372ea87f8204aa8e4512267eeb4
Natalya Botsman <botsmannatashaa@gmail.com>
2025-07-02 17:21:07 +0600

    Wrapped Home component into Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-export default function App() {
+import Home from '@/containers/Home/Home.tsx';
+import Layout from '@/layout/Layout.tsx';
 
+export default function App() {
   return (
-    <>
-    </>
+    <Layout>
+      <Home/>
+    </Layout>
   )
 };
