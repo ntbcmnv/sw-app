f412e95d392eee5b9f4d9225e7457c9d47e7df01
Natalya Botsman <botsmannatashaa@gmail.com>
2025-07-03 03:13:20 +0600

    Refactor ratesSlice: remove all unused variables

--- src/store/slices/ratesSlice.ts
+++ src/store/slices/ratesSlice.ts
@@ -3,25 +3,15 @@ import { createSlice } from '@reduxjs/toolkit';
 import { fetchRates } from '@/store/thunks/ratesThunk.ts';
 import type { RootState } from '@/app/store.ts';
 
-
 interface RatesState {
   rates: BankRate[];
-  rate: BankRate | null;
-  isLoading: boolean;
-  error: boolean;
 }
 
 const initialState: RatesState = {
   rates: [],
-  rate: null,
-  isLoading: false,
-  error: false,
 };
 
 export const selectAllRates = (state: RootState) => state.rates.rates;
-export const selectOneRate = (state: RootState) => state.rates.rate;
-export const selectRateLoading = (state: RootState) => state.rates.isLoading;
-export const selectRateError = (state: RootState) => state.rates.error;
 
 export const ratesSlice = createSlice({
   name: 'rates',
@@ -30,20 +20,16 @@ export const ratesSlice = createSlice({
   extraReducers: (builder) => {
     builder
       .addCase(fetchRates.pending, state => {
-        state.isLoading = true
-        state.error = false
+        state.rates = []
       })
 
-    .addCase(fetchRates.fulfilled, (state, {payload}) => {
-      state.isLoading = false
-      state.rates = payload;
-      state.error = false
-    })
+      .addCase(fetchRates.fulfilled, (state, {payload}) => {
+        state.rates = payload;
+      })
 
       .addCase(fetchRates.rejected, state => {
-      state.isLoading = false
-      state.error = true
-    })
+        state.rates = []
+      })
   }
 });
 
