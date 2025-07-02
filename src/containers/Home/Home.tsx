e5b06074a35835fd48ac4d7a6932229817ea6d89
Natalya Botsman <botsmannatashaa@gmail.com>
2025-07-03 03:18:11 +0600

    Update the Home component by wrapping it into Layout and removing chart headers

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,19 +1,24 @@
 import CreditsDepositsChart from '@/components/CreditsDepositsChart/CreditsDepositsChart.tsx';
 import ClientsTypesChart from '@/components/ClientsTypesChart/ClientsTypesChart.tsx';
 import ClientsAmountChart from '@/components/ClientsAmountChart/ClientsAmountChart.tsx';
-import BanksCards from '@/containers/BanksCards/BanksCards.tsx';
+import Layout from '@/layout/Layout.tsx';
+import React from 'react';
+import Banks from '@/components/Banks/Banks.tsx';
 
-const Home = () => {
+interface Props {
+  isLoggedIn: boolean;
+  setIsLoggedIn: (login: boolean) => void;
+}
+
+const Home: React.FC<Props> = ({isLoggedIn, setIsLoggedIn}) => {
   return (
-    <>
+    <Layout isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}>
       <div className="flex flex-wrap justify-center gap-6 my-6">
         <div className="w-full max-w-[500px]">
-          <h3 className="font-bold text-sm ms-2 sm:ms-0">Кредиты и Депозиты</h3>
           <CreditsDepositsChart/>
         </div>
 
         <div className="w-full max-w-[500px]">
-          <h3 className="font-bold text-sm ms-2 sm:ms-0">Распределение клиентов по типам</h3>
           <ClientsTypesChart/>
         </div>
 
@@ -23,8 +28,8 @@ const Home = () => {
         </div>
       </div>
 
-      <BanksCards/>
-    </>
+      <Banks/>
+    </Layout>
   );
 };
 
