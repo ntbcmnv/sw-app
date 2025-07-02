ca184909270e9439d10da270e67775127ca04fa2
Natalya Botsman <botsmannatashaa@gmail.com>
2025-07-02 15:37:06 +0600

    Created globals.ts to get flags due to the currency

---
+++ src/helpers/globals.ts
@@ -0,0 +1,22 @@
+export const currencyData = {
+  USD: {
+    name: 'USD',
+    flag: 'https://flagcdn.com/us.svg',
+    key: 'usd',
+  },
+  EUR: {
+    name: 'EUR',
+    flag: 'https://flagcdn.com/eu.svg',
+    key: 'eur',
+  },
+  RUB: {
+    name: 'RUB',
+    flag: 'https://flagcdn.com/ru.svg',
+    key: 'rub',
+  },
+  KZT: {
+    name: 'KZT',
+    flag: 'https://flagcdn.com/kz.svg',
+    key: 'kzt',
+  }
+};
