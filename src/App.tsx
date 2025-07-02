6fe74c65135af514ffb8c85f2fff1818dc848622
Natalya Botsman <botsmannatashaa@gmail.com>
2025-07-03 03:12:44 +0600

    Add routes for the sidebar links so the app won't crash

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import Home from '@/containers/Home/Home.tsx';
-import Layout from '@/layout/Layout.tsx';
+import { Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
 
 export default function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
-    <Layout>
-      <Home/>
-    </Layout>
+    <>
+      <Routes>
+        <Route path="/" element={<Home isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>}/>
+        <Route path="/declarations" element={<Home isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>}/>
+        <Route path="/payments-history" element={<Home isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>}/>
+        <Route path="/create-payment" element={<Home isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>}/>
+        <Route path="/payments-template" element={<Home isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>}/>
+        <Route path="/benefits" element={<Home isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>}/>
+        <Route path="/reports" element={<Home isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>}/>
+      </Routes>
+    </>
   )
 };
